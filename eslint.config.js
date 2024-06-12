5dc039480200e4b588028dc6b8503ac4bd849bb8
Alec McEachran <alecmce@gmail.com>
2024-06-12 12:44:29 +0100

    Adds eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-  {
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off"
-    },
-  }
+  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  pluginReactConfig,
+  { settings: { react: { version: "detect" } } },
+  { ignores: ["dist/*"] },
+  { rules: { 'react/react-in-jsx-scope': 'off' } },
 ];
